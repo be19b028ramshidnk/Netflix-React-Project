1cde1c18838844544dbef7ff852fcb1f341e9c6f
RAMSHID N K <ramshidnk007@gmail.com>
2024-08-18 22:47:03 +0530

    Completed page, action etc

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -3,6 +3,7 @@ import NavBar from "./Components/NavBar/navbar";
 import Banner from "./Components/Banner/Banner";
 import './App.css'
 import RowPost from "./Components/RowPost/RowPost";
+import {action,ActionMovies,ComedyMovies,Documentaries,HorrorMovies,originals, RomanceMovies} from './urls'
 
 
 function App() {
@@ -10,11 +11,20 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner />
-      <RowPost />
-      
+      <RowPost url={originals} title='Netflix Originals' />
+      <RowPost url={action} title = 'Action Movies' isSmall= {true} />
+      <RowPost url={ComedyMovies} title = 'Comedy Movies' isSmall= {true} />
+      <RowPost url={HorrorMovies} title='Horror Movies' />
+      <RowPost url={RomanceMovies} title = 'Romance Movies' isSmall= {true} />
+      <RowPost url={ActionMovies} title = 'Action Movies' isSmall= {true} />
+      <RowPost url={Documentaries} title = 'Documentaries' isSmall= {true} />
+
+
+
 
       
-      
+
+  
     </div>
   );
 }
---
+++ netflix/src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl ='https://api.themoviedb.org/3';
+export const API_KEY = "24b3978c0aa5d04eab10012e27fc57cb"
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
